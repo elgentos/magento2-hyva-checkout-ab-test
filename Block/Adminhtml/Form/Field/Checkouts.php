be2aa4971ebb294c309228e656f5a392d1f4d056
Peter Jaap Blaakmeer <peterjaap@elgentos.nl>
2022-11-10 13:03:18 +0100

    Added config fields, added frontend model, not in working state (#1)
    Added configuration fields

---
+++ Block/Adminhtml/Form/Field/Checkouts.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Elgentos\HyvaCheckoutABTest\Block\Adminhtml\Form\Field;
+
+use Hyva\CheckoutCore\Block\Adminhtml\Element\FieldArray\TypeRenderer;
+use Magento\Backend\Block\Template\Context;
+use \Magento\Config\Block\System\Config\Form\Field\FieldArray\AbstractFieldArray;
+use Magento\Framework\View\Helper\SecureHtmlRenderer;
+
+class Checkouts extends AbstractFieldArray
+{
+    private \Magento\Framework\View\Element\BlockFactory $blockFactory;
+    private \Hyva\CheckoutCore\Model\Config\Source\Checkout $checkoutSource;
+
+    public function __construct(
+        Context $context,
+        \Magento\Framework\View\Element\BlockFactory $blockFactory,
+        \Hyva\CheckoutCore\Model\Config\Source\Checkout $checkoutSource,
+        array $data = [],
+        ?SecureHtmlRenderer $secureRenderer = null
+    ) {
+        parent::__construct($context, $data, $secureRenderer);
+        $this->blockFactory = $blockFactory;
+        $this->checkoutSource = $checkoutSource;
+    }
+
+    protected function _prepareToRender()
+    {
+        $checkoutOptions = array_combine(
+            array_column($this->checkoutSource->toOptionArray(), 'value'),
+            array_column($this->checkoutSource->toOptionArray(), 'label')
+        );
+        $this->addColumn('checkout', [
+            'label' => __('Checkout'),
+            'class' => 'required-entry',
+            'renderer' => $this->blockFactory->createBlock(TypeRenderer::class)
+                ->setElementType('select')
+                ->setElementOptions($checkoutOptions),
+            'style' => 'width: 150px'
+        ]);
+        $this->addColumn('percentage', ['label' => __('Percentage Shown During Test'), 'class' => 'required-entry']);
+        $this->_addAfter = false;
+        $this->_addButtonLabel = __('Add Checkout');
+    }
+}
